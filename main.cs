656cb374bc67e11e72e1b3410db2f417cdf77245
Cuba4 <68750194+Cuba4@users.noreply.github.com>
2021-03-06 16:42:14 +0300

    v.1.2. Release + .exe Build

--- main.cs
+++ main.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Threading.Tasks;
+using System.Threading;
 class Calc {
   static void Main() {
-    Console.WriteLine("Hi! Welcome to my C# Calculator! (v.1.2 Beta)");
-    Console.WriteLine("ChangeLog: Added divide and multiply, soon i will create compiled .exe and Python version!");
+    Console.WriteLine("Hi! Welcome to my C# Calculator! (v.1.2)");
+    System.Threading.Thread.Sleep(500);
+    Console.WriteLine("ChangeLog: Added divide and multiply, Python version \"Coming Soon\"");
+    System.Threading.Thread.Sleep(500);
     Console.WriteLine("So, let's get started! Write first number.");
     double num1 = Convert.ToDouble(Console.ReadLine());
     Console.WriteLine("Ok, now, second number!");
@@ -18,22 +22,32 @@ class Calc {
         case "a":
             double result = num1 + num2;
             Console.WriteLine("Your result is: " + result + "!");
+            Console.WriteLine("Press any key to close app...");
+            Console.Read();
             break;
         case "s":
             result = num1 - num2;
             Console.WriteLine("Your result is: " + result + "!");
+            Console.WriteLine("Press any key to close app...");
+            Console.Read();
             break;
         case "d":
             result = num1 / num2;
             Console.WriteLine("Your result is: " + result + "!");
+            Console.WriteLine("Press any key to close app...");
+            Console.Read();
             break;
         case "m":
             result = num1 * num2;
             Console.WriteLine("Your result is: " + result + "!");
+            Console.WriteLine("Press any key to close app...");
+            Console.Read();
             break;
         default:
-            Console.WriteLine("You write wrong symbol! Please try again!");
+            Console.WriteLine("You wrote wrong symbol! Please try again!");
+            Console.WriteLine("Press any key to close app...");
+            Console.Read();
             break;
         }
     }
-  }
+}
